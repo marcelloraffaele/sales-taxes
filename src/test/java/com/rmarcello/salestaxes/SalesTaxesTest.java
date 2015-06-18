4c321810fd1fb6cd4b197b2984b38471e18c8221
Raffaele <Raffaele@Raffaele-HP>
2015-06-18 19:21:14 +0200

    first update

---
+++ src/test/java/com/rmarcello/salestaxes/SalesTaxesTest.java
@@ -0,0 +1,19 @@
+package com.rmarcello.salestaxes;
+
+
+
+
+import junit.framework.TestCase;
+
+
+public class SalesTaxesTest extends TestCase{
+
+public static final String HOST = "localhost";
+    public static final int PORT = 5002;
+    
+    
+	public void testA() {
+		
+        }
+	
+}
